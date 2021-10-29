73beab5893d32a3d2235fcaaf04ce2c9413760c0
hannahammonds <90658994+hannahammonds@users.noreply.github.com>
2021-10-28 20:33:05 -0500

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'basic-angular-http-request-exercises';
+  posts: {title: string, genre: string}[] = [];
+
+  constructor (private http: HttpClient){
+
+  }
+
+  onCreateMovie(form: NgForm) {
+    const {title, genre} = form.value
+    this.http.post(
+      'https://movies-project-833c7-default-rtdb.firebaseio.com/movies.json',
+      {title, genre})
+      .subscribe(responseData =>{
+        console.log(responseData)
+      })
+
+    console.log(title, genre);
+
+  }
+
+  onRetrieveMovie() {
+    this.http.get(
+      'https://movies-project-833c7-default-rtdb.firebaseio.com/movies.json',
+      )
+      .pipe(map((responseData: any) => {
+        const PostsArray = [];
+        for (const key in responseData) {
+          PostsArray.push(responseData[key])
+        }
+        return PostsArray;
+      }))
+      .subscribe(posts => {
+        console.log(posts);
+        this.posts = posts
+      })
+
+
+  }
+}
